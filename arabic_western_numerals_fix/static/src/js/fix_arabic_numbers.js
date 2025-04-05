292c3623ce4dd4eb7a0bd8979899e48729286cb2
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-05 14:44:02 +0300

    Update fix_arabic_numbers.js

--- arabic_western_numerals_fix/static/src/js/fix_arabic_numbers.js
+++ arabic_western_numerals_fix/static/src/js/fix_arabic_numbers.js
@@ -1,8 +1,10 @@
-/** Fix Arabic numerals to Western in Arabic UI (Odoo v18) **/
 odoo.define('arabic_western_numerals_fix.fix_arabic_numbers', function(require) {
     "use strict";
 
     const core = require('web.core');
+    const field_utils = require('web.field_utils');
+    const luxon = require('luxon');
+
     const _t = core._t;
 
     const eastern = ['٠','١','٢','٣','٤','٥','٦','٧','٨','٩'];
@@ -13,9 +15,23 @@ odoo.define('arabic_western_numerals_fix.fix_arabic_numbers', function(require) 
         return str.replace(/[٠-٩]/g, d => western[eastern.indexOf(d)]);
     }
 
-    const patched_t = function(str) {
+    // Patch translations
+    core._t = function (str) {
         return convertArabicToWestern(_t(str));
     };
 
-    core._t = patched_t;
+    // Patch formatted datetime (Luxon output)
+    const original_formatDate = field_utils.format.datetime;
+    field_utils.format.datetime = function (value, options) {
+        const res = original_formatDate(value, options);
+        return convertArabicToWestern(res);
+    };
+
+    // Patch float/number formatting if needed
+    const original_formatFloat = field_utils.format.float;
+    field_utils.format.float = function (value, options) {
+        const res = original_formatFloat(value, options);
+        return convertArabicToWestern(res);
+    };
+
 });
