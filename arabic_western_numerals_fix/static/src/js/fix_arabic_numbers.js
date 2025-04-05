bcf5b85b0cdba8b773ce74a5945b46ad2bb9952b
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-05 14:45:41 +0300

    Update fix_arabic_numbers.js

--- arabic_western_numerals_fix/static/src/js/fix_arabic_numbers.js
+++ arabic_western_numerals_fix/static/src/js/fix_arabic_numbers.js
@@ -1,37 +1,28 @@
-odoo.define('arabic_western_numerals_fix.fix_arabic_numbers', function(require) {
-    "use strict";
+/** @odoo-module **/
+import { _t } from "@web/core/l10n/translation";
+import { format } from "@web/core/utils/formatters";
+import { patch } from "@web/core/utils/patch";
 
-    const core = require('web.core');
-    const field_utils = require('web.field_utils');
-    const luxon = require('luxon');
+const eastern = ['٠','١','٢','٣','٤','٥','٦','٧','٨','٩'];
+const western = ['0','1','2','3','4','5','6','7','8','9'];
 
-    const _t = core._t;
+function convertArabicToWestern(str) {
+    if (typeof str !== 'string') return str;
+    return str.replace(/[٠-٩]/g, d => western[eastern.indexOf(d)]);
+}
 
-    const eastern = ['٠','١','٢','٣','٤','٥','٦','٧','٨','٩'];
-    const western = ['0','1','2','3','4','5','6','7','8','9'];
-
-    function convertArabicToWestern(str) {
-        if (typeof str !== 'string') return str;
-        return str.replace(/[٠-٩]/g, d => western[eastern.indexOf(d)]);
-    }
-
-    // Patch translations
-    core._t = function (str) {
-        return convertArabicToWestern(_t(str));
+// Patch translations (_t)
+patch(_t, 'arabic_western_numerals_fix._t', (original) => {
+    return (str, ...args) => {
+        const translated = original(str, ...args);
+        return convertArabicToWestern(translated);
     };
+});
 
-    // Patch formatted datetime (Luxon output)
-    const original_formatDate = field_utils.format.datetime;
-    field_utils.format.datetime = function (value, options) {
-        const res = original_formatDate(value, options);
-        return convertArabicToWestern(res);
+// Patch number/date formatter
+patch(format, 'arabic_western_numerals_fix.format', (original) => {
+    return (value, type, options) => {
+        const result = original(value, type, options);
+        return convertArabicToWestern(result);
     };
-
-    // Patch float/number formatting if needed
-    const original_formatFloat = field_utils.format.float;
-    field_utils.format.float = function (value, options) {
-        const res = original_formatFloat(value, options);
-        return convertArabicToWestern(res);
-    };
-
 });
