8014e40f47d9d594476f158a65e9a6f9b3b3d470
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-08 01:40:41 +0300

    Update account_move_line.py

--- account_move_running_balance/models/account_move_line.py
+++ account_move_running_balance/models/account_move_line.py
@@ -5,45 +5,61 @@ class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
     running_balance = fields.Monetary(
-        string="الرصيد التراكمي",
+        string="Running Balance",
         store=False,
         compute="_compute_running_balance",
         currency_field="currency_id",
     )
 
-    running_balance_currency = fields.Monetary(
-        string="الرصيد التراكمي بالعملة",
-        store=False,
-        compute="_compute_running_balance",
-        currency_field="currency_id",
-    )
-
-    @api.depends('custom_amount', 'amount_currency', 'account_id', 'company_id', 'partner_id', 'currency_id')
     def _compute_running_balance(self):
-        # اجمع كل السجلات المطلوبة لجميع self
-        all_account_ids = self.mapped('account_id').ids
-        company_ids = self.mapped('company_id').ids
+        if not self:
+            return
 
-        # خزن النتائج لكل سجل
-        balance_map = {}
-        currency_balance_map = {}
+        # اجمع كل السطور الحالية
+        ids_set = set(self.ids)
+        lines_map = {line.id: line for line in self}
 
-        # اجلب كل السجلات المرتبطة (لضمان دقة الرصيد)
-        all_lines = self.env['account.move.line'].search([
-            ('account_id', 'in', all_account_ids),
-            ('company_id', 'in', company_ids),
-        ], order='id ASC')  # نفس ترتيب Odoo
+        # اجمع الحسابات والشركات والعملات
+        all_account_ids = list(set(self.mapped('account_id.id')))
+        all_company_ids = list(set(self.mapped('company_id.id')))
+        all_currency_ids = list(set(self.mapped('currency_id.id')))
 
-        balance = 0.0
-        balance_currency = 0.0
+        # SQL لحساب الرصيد التراكمي بحسب ترتيب أودو الرسمي
+        self.env.cr.execute("""
+            SELECT
+                aml.id,
+                aml.account_id,
+                aml.company_id,
+                aml.currency_id,
+                aml.partner_id,
+                aml.custom_amount,
+                aml.amount_currency,
+                SUM(aml.custom_amount) OVER (
+                    PARTITION BY aml.account_id, aml.company_id, aml.currency_id,
+                        CASE WHEN aa.account_type IN ('asset_receivable', 'liability_payable') THEN aml.partner_id ELSE NULL END
+                    ORDER BY aml.date, aml.move_id, aml.id
+                ) AS running_balance,
+                SUM(aml.amount_currency) OVER (
+                    PARTITION BY aml.account_id, aml.company_id, aml.currency_id,
+                        CASE WHEN aa.account_type IN ('asset_receivable', 'liability_payable') THEN aml.partner_id ELSE NULL END
+                    ORDER BY aml.date, aml.move_id, aml.id
+                ) AS running_balance_currency
+            FROM account_move_line aml
+            JOIN account_account aa ON aml.account_id = aa.id
+            WHERE aml.account_id = ANY(%s)
+              AND aml.company_id = ANY(%s)
+              AND aml.currency_id = ANY(%s)
+              AND aml.id = ANY(%s)
+        """, (
+            all_account_ids,
+            all_company_ids,
+            all_currency_ids,
+            list(ids_set),
+        ))
 
-        for line in all_lines:
-            balance += line.custom_amount or 0.0
-            balance_currency += line.amount_currency or 0.0
-            balance_map[line.id] = balance
-            currency_balance_map[line.id] = balance_currency
+        result = self.env.cr.fetchall()
+        balance_map = {r[0]: (r[7] or 0.0) for r in result}
 
-        # عيّن الرصيد فقط للسجلات المعروضة الآن (self)
-        for rec in self:
-            rec.running_balance = balance_map.get(rec.id, 0.0)
-            rec.running_balance_currency = currency_balance_map.get(rec.id, 0.0)
+        # نعيّن القيمة فقط للسجلات الظاهرة حالياً
+        for line in self:
+            line.running_balance = balance_map.get(line.id, 0.0)
