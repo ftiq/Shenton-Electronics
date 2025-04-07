e33025ba3b72600154a83d3ddd5d9dc4fd223b70
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-08 02:25:02 +0300

    Update account_move_line.py

--- account_running_balance/models/account_move_line.py
+++ account_running_balance/models/account_move_line.py
@@ -3,29 +3,26 @@ from odoo import models, fields, api
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    previous_balance = fields.Monetary(
-        string="الرصيد السابق",
-        compute='_compute_previous_balance',
+    running_balance_line = fields.Monetary(
+        string='الرصيد التراكمي',
+        compute='_compute_running_balance_line',
         store=False,
-        currency_field='currency_id',
+        currency_field='currency_id'
     )
 
-    @api.depends('debit', 'credit', 'account_id')
-    def _compute_previous_balance(self):
-        lines = self.search(
-            [('account_id', 'in', self.mapped('account_id').ids)],
-            order="create_date, id"
-        )
+    @api.depends('move_id', 'account_id', 'date')
+    def _compute_running_balance_line(self):
+        for record in self:
+            domain = [
+                ('account_id', '=', record.account_id.id),
+                ('company_id', '=', record.company_id.id),
+            ]
+            all_lines = self.env['account.move.line'].search(domain, order='id')
 
-        balance_dict = {}
-        cumulative = {}
-
-        for line in lines:
-            acc_id = line.account_id.id
-            if acc_id not in cumulative:
-                cumulative[acc_id] = 0.0
-            balance_dict[line.id] = cumulative[acc_id]
-            cumulative[acc_id] += line.debit - line.credit
-
-        for rec in self:
-            rec.previous_balance = balance_dict.get(rec.id, 0.0)
+            running_total = 0.0
+            for line in all_lines:
+                custom_amount = line.debit - line.credit
+                running_total += custom_amount
+                if line.id == record.id:
+                    record.running_balance_line = running_total
+                    break
