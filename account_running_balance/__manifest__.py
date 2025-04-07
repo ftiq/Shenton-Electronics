63d9e378a53bc61e6f4090bce07e77d6155ca0f2
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-08 02:53:07 +0300

    Add files via upload

---
+++ account_running_balance/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Running Balance in Move Line",
+    "version": "1.0",
+    "category": "Accounting",
+    "depends": ["account"],
+    "data": ["views/account_move_line_view.xml"],
+    "installable": True,
+    "license": "LGPL-3"
+}
---
+++ account_running_balance/models/account_move_line.py
@@ -0,0 +1,44 @@
+from odoo import api, fields, models
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    running_balance = fields.Monetary(
+        string="الرصيد التراكمي",
+        store=False,
+        compute="_compute_running_balance",
+        currency_field="currency_id",
+    )
+
+    running_balance_currency = fields.Monetary(
+        string="الرصيد التراكمي بالعملة",
+        store=False,
+        compute="_compute_running_balance",
+        currency_field="currency_id",
+    )
+
+    @api.depends('custom_amount', 'amount_currency', 'account_id', 'company_id', 'partner_id', 'currency_id')
+    def _compute_running_balance(self):
+        all_account_ids = self.mapped('account_id').ids
+        company_ids = self.mapped('company_id').ids
+
+        balance_map = {}
+        currency_balance_map = {}
+
+        all_lines = self.env['account.move.line'].search([
+            ('account_id', 'in', all_account_ids),
+            ('company_id', 'in', company_ids),
+        ], order='id ASC')
+
+        balance = 0.0
+        balance_currency = 0.0
+
+        for line in all_lines:
+            balance += line.custom_amount or 0.0
+            balance_currency += line.amount_currency or 0.0
+            balance_map[line.id] = balance
+            currency_balance_map[line.id] = balance_currency
+
+        for rec in self:
+            rec.running_balance = balance_map.get(rec.id, 0.0)
+            rec.running_balance_currency = currency_balance_map.get(rec.id, 0.0)
