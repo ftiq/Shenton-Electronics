d7080e2fed4fa427e67b07a3e4b06eefde754601
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-08 02:04:56 +0300

    Update account_move_line.py

--- account_running_balance/models/account_move_line.py
+++ account_running_balance/models/account_move_line.py
@@ -3,49 +3,31 @@ from odoo import api, fields, models
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    running_balance = fields.Monetary(
-        string="Running Balance",
+    previous_balance = fields.Monetary(
+        string="الرصيد السابق",
         store=False,
-        compute="_compute_running_balance",
+        compute="_compute_previous_balance",
         currency_field="currency_id",
     )
 
-    @api.depends('custom_amount', 'account_id', 'company_id', 'currency_id', 'partner_id')
-    def _compute_running_balance(self):
-        if not self:
-            return
+    @api.depends('debit', 'credit', 'account_id')
+    def _compute_previous_balance(self):
+        # احضر كل الحركات بهذا الحساب لاحتساب الرصيد السابق
+        all_lines = self.env['account.move.line'].search(
+            [('account_id', 'in', self.mapped('account_id').ids)],
+            order='create_date, id'  # مهم جداً الحفاظ على ترتيب أودو الطبيعي
+        )
 
-        sample = self[0]
-        account_id = sample.account_id.id
-        company_id = sample.company_id.id
-        currency_id = sample.currency_id.id
+        balances = {}
+        running = {}
 
-        is_partner_required = sample.account_id.account_type in ['asset_receivable', 'liability_payable']
-        partner_id = sample.partner_id.id if is_partner_required else None
+        for line in all_lines:
+            key = line.account_id.id
 
-        where = """
-            account_id = %s AND company_id = %s AND currency_id = %s
-        """
-        params = [account_id, company_id, currency_id]
-        if is_partner_required:
-            where += " AND partner_id = %s"
-            params.append(partner_id)
+            running.setdefault(key, 0)
+            balances[line.id] = running[key]
 
-        query = f"""
-            SELECT id, custom_amount
-            FROM account_move_line
-            WHERE {where}
-            ORDER BY id
-        """
-
-        self.env.cr.execute(query, tuple(params))
-        results = self.env.cr.fetchall()
-
-        balance = 0.0
-        balance_map = {}
-        for line_id, amount in results:
-            balance_map[line_id] = balance
-            balance += amount or 0.0
+            running[key] += line.debit - line.credit
 
         for rec in self:
-            rec.running_balance = balance_map.get(rec.id, 0.0)
+            rec.previous_balance = balances.get(rec.id, 0.0)
