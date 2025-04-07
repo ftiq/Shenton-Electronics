130d207f9ed8e11b93e1fa673869e13ea1ff1581
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-08 02:06:01 +0300

    Update account_move_line.py

--- account_running_balance/models/account_move_line.py
+++ account_running_balance/models/account_move_line.py
@@ -1,33 +1,29 @@
-from odoo import api, fields, models
+from odoo import models, fields, api
 
 class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
+    _inherit = 'account.move.line'
 
     previous_balance = fields.Monetary(
-        string="الرصيد السابق",
+        string='الرصيد السابق',
+        compute='_compute_previous_balance',
+        currency_field='currency_id',
         store=False,
-        compute="_compute_previous_balance",
-        currency_field="currency_id",
     )
 
     @api.depends('debit', 'credit', 'account_id')
     def _compute_previous_balance(self):
-        # احضر كل الحركات بهذا الحساب لاحتساب الرصيد السابق
         all_lines = self.env['account.move.line'].search(
             [('account_id', 'in', self.mapped('account_id').ids)],
-            order='create_date, id'  # مهم جداً الحفاظ على ترتيب أودو الطبيعي
+            order='create_date, id'
         )
-
-        balances = {}
-        running = {}
+        balance_dict = {}
+        running_balance = {}
 
         for line in all_lines:
-            key = line.account_id.id
-
-            running.setdefault(key, 0)
-            balances[line.id] = running[key]
-
-            running[key] += line.debit - line.credit
+            acc_id = line.account_id.id
+            running_balance.setdefault(acc_id, 0)
+            balance_dict[line.id] = running_balance[acc_id]
+            running_balance[acc_id] += line.debit - line.credit
 
         for rec in self:
-            rec.previous_balance = balances.get(rec.id, 0.0)
+            rec.previous_balance = balance_dict.get(rec.id, 0.0)
