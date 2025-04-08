2519f4f78b60027c938b0c668b98795fab872627
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-08 03:21:23 +0300

    Update account_move_line.py

--- account_running_balance/models/account_move_line.py
+++ account_running_balance/models/account_move_line.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class AccountMoveLine(models.Model):
@@ -8,30 +8,31 @@ class AccountMoveLine(models.Model):
         string="الرصيد التراكمي",
         store=False,
         compute="_compute_running_balance",
-        currency_field="currency_id"
+        currency_field="currency_id",
     )
 
     running_balance_currency = fields.Monetary(
         string="الرصيد التراكمي بالعملة",
         store=False,
         compute="_compute_running_balance",
-        currency_field="currency_id"
+        currency_field="currency_id",
     )
 
-    @api.depends('custom_amount', 'amount_currency', 'account_id', 'company_id', 'currency_id')
+    @api.depends('custom_amount', 'amount_currency', 'account_id', 'company_id', 'partner_id', 'currency_id')
     def _compute_running_balance(self):
-        # جهز الحسابات والشركات المطلوبة
+        if not self:
+            return
+
         account_ids = self.mapped('account_id').ids
         company_ids = self.mapped('company_id').ids
 
-        # اجلب كل السطور المرتبطة، مرتبة بـ id ASC (الوحيد الموثوق لترتيب دائم)
         domain = [
             ('account_id', 'in', account_ids),
-            ('company_id', 'in', company_ids)
+            ('company_id', 'in', company_ids),
         ]
+
         all_lines = self.env['account.move.line'].search(domain, order='id ASC')
 
-        # احسب الرصيد التراكمي
         balance = 0.0
         balance_currency = 0.0
         balance_map = {}
@@ -43,7 +44,6 @@ class AccountMoveLine(models.Model):
             balance_map[line.id] = balance
             currency_map[line.id] = balance_currency
 
-        # خصص الرصيد فقط للسجلات المطلوبة الآن
         for rec in self:
             rec.running_balance = balance_map.get(rec.id, 0.0)
             rec.running_balance_currency = currency_map.get(rec.id, 0.0)
