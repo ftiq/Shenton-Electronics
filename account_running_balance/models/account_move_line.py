b49c4cb36398f66929fa52eb61d003b099516c4e
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-08 03:34:10 +0300

    Update account_move_line.py

--- account_running_balance/models/account_move_line.py
+++ account_running_balance/models/account_move_line.py
@@ -26,35 +26,28 @@ class AccountMoveLine(models.Model):
         Account = self.env['account.account']
         MoveLine = self.env['account.move.line']
 
-        # تجميع الحسابات المرتبطة بكل سجل
         account_ids = self.mapped('account_id').ids
         company_ids = self.mapped('company_id').ids
 
-        # جلب نوع الحساب
+        # معرفة نوع الحساب
         account_type_map = {
             acc.id: acc.account_type
             for acc in Account.browse(account_ids)
         }
 
-        # إعداد الرصيد
-        balance_map = {}
-        currency_balance_map = {}
-
-        # جلب كافة السطور المطلوبة للحسابات المعنية
         all_lines = MoveLine.search([
             ('account_id', 'in', account_ids),
             ('company_id', 'in', company_ids),
         ], order='id ASC')
 
-        # المتغيرات المؤقتة
+        balance_map = {}
+        currency_map = {}
         grouped_balance = {}
         grouped_currency_balance = {}
 
         for line in all_lines:
             acc_type = account_type_map.get(line.account_id.id)
-            key = None
-
-            # تحديد المفتاح التجميعي بحسب نوع الحساب
+            # مفتاح التجميع حسب نوع الحساب
             if acc_type in ['receivable', 'payable']:
                 key = (line.account_id.id, line.company_id.id, line.partner_id.id)
             else:
@@ -67,9 +60,8 @@ class AccountMoveLine(models.Model):
             grouped_currency_balance[key] += line.amount_currency or 0.0
 
             balance_map[line.id] = grouped_balance[key]
-            currency_balance_map[line.id] = grouped_currency_balance[key]
+            currency_map[line.id] = grouped_currency_balance[key]
 
-        # تعيين القيم على السجلات المطلوبة
         for rec in self:
             rec.running_balance = balance_map.get(rec.id, 0.0)
-            rec.running_balance_currency = currency_balance_map.get(rec.id, 0.0)
+            rec.running_balance_currency = currency_map.get(rec.id, 0.0)
