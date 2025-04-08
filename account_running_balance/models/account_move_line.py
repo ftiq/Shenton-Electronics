220b38c40b626cdd3e09515f8cc7861932eb6b28
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-08 03:31:11 +0300

    Update account_move_line.py

--- account_running_balance/models/account_move_line.py
+++ account_running_balance/models/account_move_line.py
@@ -1,5 +1,6 @@
 from odoo import api, fields, models
 
+
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
@@ -22,27 +23,53 @@ class AccountMoveLine(models.Model):
         if not self:
             return
 
+        Account = self.env['account.account']
+        MoveLine = self.env['account.move.line']
+
+        # تجميع الحسابات المرتبطة بكل سجل
         account_ids = self.mapped('account_id').ids
         company_ids = self.mapped('company_id').ids
 
-        domain = [
+        # جلب نوع الحساب
+        account_type_map = {
+            acc.id: acc.account_type
+            for acc in Account.browse(account_ids)
+        }
+
+        # إعداد الرصيد
+        balance_map = {}
+        currency_balance_map = {}
+
+        # جلب كافة السطور المطلوبة للحسابات المعنية
+        all_lines = MoveLine.search([
             ('account_id', 'in', account_ids),
             ('company_id', 'in', company_ids),
-        ]
+        ], order='id ASC')
 
-        all_lines = self.env['account.move.line'].search(domain, order='id ASC')
-
-        balance = 0.0
-        balance_currency = 0.0
-        balance_map = {}
-        currency_map = {}
+        # المتغيرات المؤقتة
+        grouped_balance = {}
+        grouped_currency_balance = {}
 
         for line in all_lines:
-            balance += line.custom_amount or 0.0
-            balance_currency += line.amount_currency or 0.0
-            balance_map[line.id] = balance
-            currency_map[line.id] = balance_currency
+            acc_type = account_type_map.get(line.account_id.id)
+            key = None
 
+            # تحديد المفتاح التجميعي بحسب نوع الحساب
+            if acc_type in ['receivable', 'payable']:
+                key = (line.account_id.id, line.company_id.id, line.partner_id.id)
+            else:
+                key = (line.account_id.id, line.company_id.id)
+
+            grouped_balance.setdefault(key, 0.0)
+            grouped_currency_balance.setdefault(key, 0.0)
+
+            grouped_balance[key] += line.custom_amount or 0.0
+            grouped_currency_balance[key] += line.amount_currency or 0.0
+
+            balance_map[line.id] = grouped_balance[key]
+            currency_balance_map[line.id] = grouped_currency_balance[key]
+
+        # تعيين القيم على السجلات المطلوبة
         for rec in self:
             rec.running_balance = balance_map.get(rec.id, 0.0)
-            rec.running_balance_currency = currency_map.get(rec.id, 0.0)
+            rec.running_balance_currency = currency_balance_map.get(rec.id, 0.0)
