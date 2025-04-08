4b921bd269dc5510ada909732bd11181464594c6
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-08 03:16:49 +0300

    Update account_move_line.py

--- account_running_balance/models/account_move_line.py
+++ account_running_balance/models/account_move_line.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import models, fields, api
 
 
 class AccountMoveLine(models.Model):
@@ -8,54 +8,42 @@ class AccountMoveLine(models.Model):
         string="الرصيد التراكمي",
         store=False,
         compute="_compute_running_balance",
-        currency_field="currency_id",
+        currency_field="currency_id"
     )
 
     running_balance_currency = fields.Monetary(
         string="الرصيد التراكمي بالعملة",
         store=False,
         compute="_compute_running_balance",
-        currency_field="currency_id",
+        currency_field="currency_id"
     )
 
-    @api.depends('custom_amount', 'amount_currency', 'account_id', 'company_id', 'partner_id', 'currency_id')
+    @api.depends('custom_amount', 'amount_currency', 'account_id', 'company_id', 'currency_id')
     def _compute_running_balance(self):
-        Account = self.env['account.account']
-        grouped_lines = {}
+        # جهز الحسابات والشركات المطلوبة
+        account_ids = self.mapped('account_id').ids
+        company_ids = self.mapped('company_id').ids
 
-        # جلب جميع السطور المطلوبة
-        all_lines = self.env['account.move.line'].search([
-            ('account_id', 'in', self.mapped('account_id').ids),
-            ('company_id', 'in', self.mapped('company_id').ids),
-        ], order='id ASC')
+        # اجلب كل السطور المرتبطة، مرتبة بـ id ASC (الوحيد الموثوق لترتيب دائم)
+        domain = [
+            ('account_id', 'in', account_ids),
+            ('company_id', 'in', company_ids)
+        ]
+        all_lines = self.env['account.move.line'].search(domain, order='id ASC')
 
-        # التجميع حسب (partner_id + account_id) فقط إذا كان نوع الحساب يتطلب ذلك
-        for line in all_lines:
-            account_type = line.account_id.account_type
-            key = (line.account_id.id, line.company_id.id)
-
-            if account_type in ['receivable', 'payable']:
-                key = (line.partner_id.id, line.account_id.id, line.company_id.id)
-
-            if key not in grouped_lines:
-                grouped_lines[key] = []
-
-            grouped_lines[key].append(line)
-
-        # حساب الرصيد التراكمي
+        # احسب الرصيد التراكمي
+        balance = 0.0
+        balance_currency = 0.0
         balance_map = {}
         currency_map = {}
 
-        for group in grouped_lines.values():
-            balance = 0.0
-            balance_currency = 0.0
-            for line in group:
-                balance += line.custom_amount or 0.0
-                balance_currency += line.amount_currency or 0.0
-                balance_map[line.id] = balance
-                currency_map[line.id] = balance_currency
+        for line in all_lines:
+            balance += line.custom_amount or 0.0
+            balance_currency += line.amount_currency or 0.0
+            balance_map[line.id] = balance
+            currency_map[line.id] = balance_currency
 
-        # تعيين النتائج للسجلات الحالية فقط
+        # خصص الرصيد فقط للسجلات المطلوبة الآن
         for rec in self:
             rec.running_balance = balance_map.get(rec.id, 0.0)
             rec.running_balance_currency = currency_map.get(rec.id, 0.0)
