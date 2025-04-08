7eeb762f7d7a29f1fb488d488b1ead0e2d09ffe8
MHD Manar Zamrik <manar.z@ft.iq>
2025-04-08 03:28:55 +0300

    Update account_move_line.py

--- account_running_balance/models/account_move_line.py
+++ account_running_balance/models/account_move_line.py
@@ -1,6 +1,5 @@
 from odoo import api, fields, models
 
-
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
@@ -20,42 +19,30 @@ class AccountMoveLine(models.Model):
 
     @api.depends('custom_amount', 'amount_currency', 'account_id', 'company_id', 'partner_id', 'currency_id')
     def _compute_running_balance(self):
-        Account = self.env['account.account']
-        grouped_lines = {}
+        if not self:
+            return
 
-        # جلب جميع السطور المطلوبة
-        all_lines = self.env['account.move.line'].search([
-            ('account_id', 'in', self.mapped('account_id').ids),
-            ('company_id', 'in', self.mapped('company_id').ids),
-        ], order='id ASC')
+        account_ids = self.mapped('account_id').ids
+        company_ids = self.mapped('company_id').ids
 
-        # التجميع حسب (partner_id + account_id) فقط إذا كان نوع الحساب يتطلب ذلك
-        for line in all_lines:
-            account_type = line.account_id.account_type
-            key = (line.account_id.id, line.company_id.id)
+        domain = [
+            ('account_id', 'in', account_ids),
+            ('company_id', 'in', company_ids),
+        ]
 
-            if account_type in ['receivable', 'payable']:
-                key = (line.partner_id.id, line.account_id.id, line.company_id.id)
+        all_lines = self.env['account.move.line'].search(domain, order='id ASC')
 
-            if key not in grouped_lines:
-                grouped_lines[key] = []
-
-            grouped_lines[key].append(line)
-
-        # حساب الرصيد التراكمي
+        balance = 0.0
+        balance_currency = 0.0
         balance_map = {}
         currency_map = {}
 
-        for group in grouped_lines.values():
-            balance = 0.0
-            balance_currency = 0.0
-            for line in group:
-                balance += line.custom_amount or 0.0
-                balance_currency += line.amount_currency or 0.0
-                balance_map[line.id] = balance
-                currency_map[line.id] = balance_currency
+        for line in all_lines:
+            balance += line.custom_amount or 0.0
+            balance_currency += line.amount_currency or 0.0
+            balance_map[line.id] = balance
+            currency_map[line.id] = balance_currency
 
-        # تعيين النتائج للسجلات الحالية فقط
         for rec in self:
             rec.running_balance = balance_map.get(rec.id, 0.0)
             rec.running_balance_currency = currency_map.get(rec.id, 0.0)
